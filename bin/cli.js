170a8e6e6791fe03af0b7f3493f713fd21cae0b3
codingsapienss <contactprsant@gmail.com>
2025-07-17 17:57:37 +0530

    updating executable

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,17 @@
 const path = require('path');
 const { generateKeyPairAndSave } = require('../utils/generateKeys');
+const dotenv = require('dotenv');
+dotenv.config();
 
-generateKeyPairAndSave({
-    privateKeyPath: path.resolve(__dirname, '../keys/private.pem'),
-    publicKeyPath: path.resolve(__dirname, '../public/public.pem'),
-    passphrase: process.env.SECURE_PASSPHRASE || 'default-passphrase',
-});
+const command = process.argv[2];
+
+if (command === 'generate:keys') {
+    generateKeyPairAndSave({
+        privateKeyPath: path.resolve(__dirname, '../keys/private.pem'),
+        publicKeyPath: path.resolve(__dirname, '../public/public.pem'),
+        passphrase: process.env.SECURE_PASSPHRASE || 'default-passphrase',
+    });
+} else {
+    console.error(`Unknown command: ${command}`);
+    process.exit(1);
+}
